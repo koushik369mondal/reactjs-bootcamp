aa52edc0a1250ca2ea6d5bdf02768ce10a07df62
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-06 18:48:27 +0530

    Add state management to track button clicks in App component

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 
 const App = () => {
 
+  const [x, setx] = useState(0);
+
   const btnClick = () => {
     console.log("Button clicked");
+    setx(x + 1);
+    console.log(x);
   }
 
   return (
     <div>
+      {x}
       <button onClick={() => {btnClick()}}>Click me</button>
     </div>
   );
