92fa142c3cd042fde0e33bdd21d0551b4770b644
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-06 19:55:16 +0530

    Update title in index.html to "Todo List" and remove unused components and assets

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,21 +1,9 @@
-import { useRef, useState } from "react";
 
 const App = () => {
 
-  const [data, setData] = useState([]);
-  const inputRef = useRef(null);
-
   return (
     <div>
-      <input ref={inputRef} type="text" />
-      <button
-        onClick={() => {
-          setData([...data, inputRef.current.value]);
-        }}
-      >
-        Submit
-      </button>
-      {data.map((item,index) => {return <h2 key={index}>{item}</h2>})}
+      
     </div>
   );
 };
