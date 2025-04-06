5ee250aa552c19af6e8a46c3903504b1539b73b2
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-06 23:04:02 +0530

    Add Todo item images and update TodoItems component to display them

--- first-project/src/Components/TodoItems.jsx
+++ first-project/src/Components/TodoItems.jsx
@@ -1,11 +1,19 @@
-import "./CSS/TodoItems.css"
+import "./CSS/TodoItems.css";
+import tick from "./Assets/tick.png";
+import not_tick from "./Assets/not_tick.png";
+import cross from "./Assets/cross.png";
 
-const TodoItems = () => {
+const TodoItems = ({no, display, text}) => {
     return (
-        <div>
-
+        <div className="todoitems">
+            <div className="todoitems-container">
+                <img src={not_tick} alt="" />
+                <img src={tick} alt="" />
+                <div className="todoitems-text">{text}</div>
+            </div>
+            <img src={cross} alt="" />
         </div>
-    )
-}
+    );
+};
 
-export default TodoItems
+export default TodoItems;
