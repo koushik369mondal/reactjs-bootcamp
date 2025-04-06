14a5129cdd15b3b7f1bd826c6a84b1d5562fae01
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-06 23:53:12 +0530

    add delete functionality to TodoItems component and update package.json for gh-pages

--- first-project/src/Components/TodoItems.jsx
+++ first-project/src/Components/TodoItems.jsx
@@ -5,6 +5,12 @@ import cross from "./Assets/cross.png";
 
 const TodoItems = ({ no, display, text, setTodos }) => {
 
+    const deleteTodo = (no) => {
+        let data = JSON.parse(localStorage.getItem("todos"));
+        data = data.filter((todo) => todo.no !== no);
+        setTodos(data);
+    }
+
     const toggle = (no) => {
         let data = JSON.parse(localStorage.getItem("todos"));
         for(let i=0; i<data.length; i++){
@@ -31,7 +37,7 @@ const TodoItems = ({ no, display, text, setTodos }) => {
                 )}
                 <div className="todo-items-text">{text}</div>
             </div>
-            <img className="todo-items-cross-icon" src={cross} alt="" />
+            <img className="todo-items-cross-icon" onClick={() => {deleteTodo(no)}} src={cross} alt="" />
         </div>
     );
 };
