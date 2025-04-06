b3d473facbbf3be109c4586d2f9582e35b85dae8
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-06 18:39:58 +0530

    simplify rendering logic

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,15 +1,10 @@
-
 const App = () => {
-
-  let x  = "Kauhsik Mandal"
-  let array = ["user1", "user2", "user3", "user4"];
-
+  let data = "boy";
   return (
     <div>
-      {x}
-      {array.map((user) => {return <h2>{user}</h2>})}
+      {data === "boy" ? <h1>Boy</h1> : <h1>Girl</h1>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
